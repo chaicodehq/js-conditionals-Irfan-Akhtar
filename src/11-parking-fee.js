2a18b6b577f6f22c495b0ea32ba2a34cb49aac0a
Irfan Akhtar <irfanakhtar506@gmail.com>
2026-06-12 15:58:21 +0530

    Complete Conditional Lab

--- src/11-parking-fee.js
+++ src/11-parking-fee.js
@@ -34,4 +34,44 @@
  */
 export function calculateParkingFee(hours, vehicleType) {
   // Your code here
+  if (hours <= 0) {
+    return -1;
+  }
+
+  hours = Math.ceil(hours);
+
+  let firstHourRate;
+  let additionalHourRate;
+  let dailyMax;
+
+  if (vehicleType === "car") {
+    firstHourRate = 5;
+    additionalHourRate = 3;
+    dailyMax = 30;
+  } 
+  else if (vehicleType === "motorcycle") {
+    firstHourRate = 3;
+    additionalHourRate = 2;
+    dailyMax = 18;
+  } 
+  else if (vehicleType === "bus") {
+    firstHourRate = 10;
+    additionalHourRate = 7;
+    dailyMax = 60;
+  } 
+  else {
+    return -1;
+  }
+
+  let fee = firstHourRate;
+
+  if (hours > 1) {
+    fee += (hours - 1) * additionalHourRate;
+  }
+
+  if (fee > dailyMax) {
+    fee = dailyMax;
+  }
+
+  return fee;
 }
